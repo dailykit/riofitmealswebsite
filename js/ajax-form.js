5c2c813362de1e24ff60674d0f56236db66b58c4
Praveen Bisht <prvnbist@gmail.com>
2020-12-10 21:11:19 +0530

    feat: contact form func

--- js/ajax-form.js
+++ js/ajax-form.js
@@ -1,48 +1,137 @@
-$(function() {
+$(function () {
+   const fetch_query = async (query, variables) => {
+      try {
+         const response = await fetch(URL, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+               'x-hasura-admin-secret': ADMIN_SECRET
+            },
+            body: JSON.stringify({ query, variables })
+         })
+         const { data = {} } = await response.json()
+         return { success: true, data }
+      } catch (error) {
+         return { success: false, error }
+      }
+   }
+   // Get the form.
+   const form = $('#contact-us-form')
 
-	// Get the form.
-	var form = $('#contact-us-form');
+   const URL = 'https://riofitmeals.dailykit.org/datahub/v1/graphql'
+   const ADMIN_SECRET = '8b8e4dd5-243d-4060-b77b-a1934f7ed087'
 
-	// Get the messages div.
-	var formMessages = $('.ajax-response');
+   // Get the messages div.
+   const form_response = $('.ajax-response')
 
-	// Set up an event listener for the contact form.
-	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
-		e.preventDefault();
+   // Set up an event listener for the contact form.
+   $(form).submit(async function (e) {
+      e.preventDefault()
 
-		// Serialize the form data.
-		var formData = $(form).serialize();
+      let toEmail = ''
+      try {
+         const query = `
+            query emails($domain: String) {
+               emails: brands_brand_storeSetting(
+                  where: {
+                     onDemandSetting: {
+                        identifier: { _eq: "Contact" }
+                        brand: {
+                           brand: {
+                              _or: [
+                                 { domain: { _eq: $domain } }
+                                 { isDefault: { _eq: true } }
+                              ]
+                           }
+                        }
+                     }
+                  }
+               ) {
+                  email: value(path: "email")
+               }
+            }
+         `
+         const { success, ...rest } = await fetch_query(query, {
+            domain: window.location.hostname
+         })
+         if (success) {
+            const { emails = [] } = rest.data
+            if (emails.length > 0) {
+               toEmail = emails[0].email
+            } else {
+               throw rest.error
+            }
+         }
+      } catch (error) {
+         console.log('Failed fetching email to send the mail from:', error)
+      }
 
-		// Submit the form using AJAX.
-		$.ajax({
-			type: 'POST',
-			url: $(form).attr('action'),
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+      if (toEmail) {
+         const form = Object.fromEntries(new FormData(e.target))
+         const query = `
+            mutation sendEmail($emailInput: EmailInput!) {
+               sendEmail(emailInput: $emailInput) {
+                  message
+                  success
+               }
+            }
+         `
 
-			// Set the message text.
-			$(formMessages).text(response);
+         try {
+            const variables = {
+               emailInput: {
+                  to: toEmail,
+                  attachments: [],
+                  from: 'no-reply@dailykit.org',
+                  subject: `${form.name} filled your form on your website. Please check!`,
+                  html: `
+                     <h4>Name: ${form.name}</h4>
+                     <h4>Email: ${form.email}</h4>
+                     <p>Message: ${form.message}</p>
+                  `
+               }
+            }
+            const { success, ...rest } = await fetch_query(query, variables)
+            if (success) {
+               const { sendEmail = {} } = rest.data
+               if ('success' in sendEmail && sendEmail.success) {
+                  $(form_response).text(sendEmail.message)
+                  $('#contact-us-form input,#contact-us-form textarea').val('')
 
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
-
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
-		});
-	});
-
-});
+                  try {
+                     const variables = {
+                        emailInput: {
+                           to: form.email,
+                           attachments: [],
+                           from: 'no-reply@dailykit.org',
+                           subject: `From RioFitMeals`,
+                           html: `
+                              <h4>Thanks for your message.</h4>
+                              <h4>Here's your message.</h4>
+                              <p>${form.message}</p>
+                           `
+                        }
+                     }
+                     const { success, ...rest } = await fetch_query(
+                        query,
+                        variables
+                     )
+                     if (!success) {
+                        throw rest.error
+                     }
+                  } catch (error) {
+                     console.log('Failed sending to submitter:', error)
+                  }
+               }
+            } else {
+               throw rest.error
+            }
+         } catch (error) {
+            console.log('Failed sending to RioFitMeals:', error)
+            $(form_response).text(
+               'Oops! An error occured and your message could not be sent.'
+            )
+         }
+      }
+   })
+})
